f10a4b56019f0d78c508d4270b59653e3c9253b7
julia <juliarome@live.fr>
2021-01-14 21:29:23 +0100

    ajoutphoto

--- front/src/components/AvatarCard/AvatarCard.jsx
+++ front/src/components/AvatarCard/AvatarCard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./AvatarCard.css";
 // import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import logo from './planting.png';
 
 function AvatarCard({ idAgriculteur, farmSize /*, historiqueTransaction**/ }) {
   return (
     <div>
       <div className="card-container">
         <div className="TopSection">
+        {/* <img src={logo} alt="logo" className="logo" /> */}
           <h1 classNameme="AgriculteurTitle">Agriculteur n°{idAgriculteur}</h1>
           <div className="FarmSizeTheme">
             <h2>Taille ferme: {farmSize} hectares</h2>
@@ -17,10 +19,6 @@ function AvatarCard({ idAgriculteur, farmSize /*, historiqueTransaction**/ }) {
         <div className="transaction">
           <h2>Historique des transactions</h2>
         </div>
-
-        {/* <Link to="/Transaction">
-            <img src={TransactionTab} alt="TransactionTab" className="logo" />
-          </Link> */}
       </div>
     </div>
   );
@@ -33,22 +31,3 @@ AvatarCard.propTypes = {
 };
 
 export default AvatarCard;
-
-// const Avatar = () => {
-// }
-// {
-//      return (
-// <div className="Card">
-//  <div className="Top-Card">
-//   <h3>Avatar</h3>
-//       <h2>Agriculteur 3</h2>
-//        <h3>taille ferme</h3>
-//  </div>
-//    <div className="ligne_horizontal">
-//        <h2>Historique des transactions</h2>
-//       </div>
-//     </div>
-//   );
-//  };
-
-//  export default Avatar;
